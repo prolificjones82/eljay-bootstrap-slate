ce84566236c8b33d9c97f53cffb4bd853473f2fd
Lee Jones <mail@leejones.me.uk>
2019-09-04 21:23:59 +0100

    Password layout

--- src/scripts/layout/theme.js
+++ src/scripts/layout/theme.js
@@ -9,22 +9,94 @@ import '../../styles/theme.scss';
 import '../../styles/theme.scss.liquid';
 
 import $ from 'jquery';
-import 'bootstrap';
-import { focusHash, bindInPageLinks } from '@shopify/theme-a11y';
+import 'bootstrap/js/dist/modal';
+import { forceFocus, bindInPageLinks } from '@shopify/theme-a11y';
 import { cookiesEnabled } from '@shopify/theme-cart';
+import Cookies from 'js-cookie';
+import { isMobile } from 'mobile-device-detect';
 
-// Common a11y fixes
-focusHash();
-bindInPageLinks();
+window.slate = window.slate || {};
+window.theme = window.theme || {};
+window.Shopify = window.Shopify || {};
 
-// Apply a specific class to the html element for browser support of cookies.
-if (cookiesEnabled()) {
-    document.documentElement.className = document.documentElement.className.replace(
-        'supports-no-cookies',
-        'supports-cookies',
-    );
+slate.utils = {
+    getParameterByName: function(name, url) {
+        if (!url) url = window.location.href;
+        name = name.replace(/[[\]]/g, '\\$&');
+        var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+          results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, ' '));
+    },
+    keyboardKeys: {
+        TAB: 9,
+        ENTER: 13,
+        LEFTARROW: 37,
+        RIGHTARROW: 39
+    }
+};
+
+theme.selectors = {
+    mainContent: '#mainContent'
+};
+
+theme.breakpoints = {
+    'sm':     parseInt(getComputedStyle(document.documentElement).getPropertyValue('--breakpoint-sm'), 10),
+    'md':     parseInt(getComputedStyle(document.documentElement).getPropertyValue('--breakpoint-md'), 10),
+    'lg':     parseInt(getComputedStyle(document.documentElement).getPropertyValue('--breakpoint-lg'), 10),
+    'xl':     parseInt(getComputedStyle(document.documentElement).getPropertyValue('--breakpoint-xl'), 10)
 }
 
-$(document).ready(() => {
+$(window).on('load', () => {
+    $('body').addClass('loaded');
+});
 
+$(function() {
+    // catch for reviews not working on localhost during development
+    if (window.Shopify.shop.includes("localhost")) {
+        window.Shopify.shop = window.theme.shopUrl;
+    }
+
+    bindInPageLinks();
+
+    $('.in-page-link').on('click', (evt) => {
+        forceFocus($(evt.currentTarget.hash));
+    });
+
+    // Apply a specific class to the html element for browser support of cookies.
+    if (navigator.cookieEnabled) {
+        document.documentElement.className = document.documentElement.className.replace(
+            'supports-no-cookies',
+            'supports-cookies',
+        );
+    }
+
+    $('.toggle-password').on('click tap', (evt) => {
+        evt.preventDefault();
+        $('body').toggleClass('password-open');
+    });
+
+    $('.toggle-search').on('click tap', (evt) => {
+        evt.preventDefault();
+        $('body').toggleClass('search-open');
+    });
+
+    $('.toggle-navigation').on('click tap', (evt) => {
+        evt.preventDefault();
+        $('body').toggleClass('menu-open');
+    });
+
+    $('.toggle-link').on('click tap', (evt) => {
+        evt.preventDefault();
+        var target = $(evt.currentTarget).data('target');
+        $(target).toggleClass('hide');
+    });
+
+    $('.back-to-top').on('click tap', (evt) => {
+        evt.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        }, 800);
+    });
 });
